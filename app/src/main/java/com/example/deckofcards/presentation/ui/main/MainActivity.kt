6030d51fab05cd1c6546043aa1ea2c8ac2398302
jefteFelip <jefte.felipe@hotmail.com>
2024-08-14 17:20:48 -0300

    implementatin binding in view

--- app/src/main/java/com/example/deckofcards/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/deckofcards/presentation/ui/main/MainActivity.kt
@@ -1,13 +1,10 @@
 package com.example.deckofcards.presentation.ui.main
 
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.example.deckofcards.R
+import com.example.deckofcards.databinding.ActivityMainBinding
 import com.example.deckofcards.presentation.ui.adapter.CardsAdapter
 import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -15,25 +12,23 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 class MainActivity : AppCompatActivity() {
     private val viewModel: CardsViewModel by viewModel()
     private lateinit var adapter: CardsAdapter
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         adapter = CardsAdapter()
 
-        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = adapter
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = adapter
 
-        viewModel.cards.observe(
-            this,
-            Observer { cards ->
-                adapter.submitList(cards)
-            },
-        )
+        viewModel.cards.observe(this) { cards ->
+            adapter.submitList(cards)
+        }
 
-        findViewById<Button>(R.id.reshuffleButton).setOnClickListener {
+        binding.reshuffleButton.setOnClickListener {
             lifecycleScope.launch {
                 viewModel.deckId.value?.let { deckId ->
                     viewModel.reshuffleDeck(deckId)
